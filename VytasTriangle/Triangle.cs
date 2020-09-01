9c94bde156594756021d4d5e7deb8c2731ad99cd
Opilane TTHK <opilane@tthk.ee>
2020-09-01 11:15:10 +0300

    Add project files.

---
+++ VytasTriangle/Triangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VytasTriangle
+{
+    public class Triangle
+    {
+        public double a;
+        public double b;
+        public double c;
+
+        public Triangle(double a, double b, double c)
+        {
+
+        }
+    }
+}
